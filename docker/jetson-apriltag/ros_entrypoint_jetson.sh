1bbd824c0f3a90fc926700e44f987bdbc7321e31
itdependz <samarthdas81@gmail.com>
2024-02-08 03:46:28 +0000

    minor build changes

--- docker/jetson-apriltag/ros_entrypoint_jetson.sh
+++ docker/jetson-apriltag/ros_entrypoint_jetson.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-set -e
+# set -e
 
-# setup ros2 environment
-source "/opt/ros/$ROS_DISTRO/install/setup.bash" --
-source "/root/ros2_ws/install/local_setup.bash" --
+# # setup ros2 environment
+# source "/opt/ros/$ROS_DISTRO/install/setup.bash" --
+# source "/root/ros2_ws/install/local_setup.bash" --
 
-# Welcome information
-echo "ZED ROS2 Docker Image"
-echo "---------------------"
-echo 'ROS distro: ' $ROS_DISTRO
-echo 'DDS middleware: ' $RMW_IMPLEMENTATION 
-echo "---"  
-echo 'Available ZED packages:'
-ros2 pkg list | grep zed
-echo "---------------------"    
-exec "$@"
+# # Welcome information
+# echo "ZED ROS2 Docker Image"
+# echo "---------------------"
+# echo 'ROS distro: ' $ROS_DISTRO
+# echo 'DDS middleware: ' $RMW_IMPLEMENTATION 
+# echo "---"  
+# echo 'Available ZED packages:'
+# ros2 pkg list | grep zed
+# echo "---------------------"    
+# exec "$@"
