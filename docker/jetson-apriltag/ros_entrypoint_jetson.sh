7a04276dc86147716cc90bc1cc6517e7c1563204
ugackMiner53 <ugackMiner@gmail.com>
2024-02-13 00:35:41 +0000

    it dont work because aaaaaaa why does the package not exist

--- docker/jetson-apriltag/ros_entrypoint_jetson.sh
+++ docker/jetson-apriltag/ros_entrypoint_jetson.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /opt/ros/${ROS_DISTRO}/setup.bash
-source install/setup.bash
+source /opt/ros/${ROS_DISTRO}/install/setup.bash
+source ./src/install/setup.bash
 
 ros2 launch isaac_ros_apriltag isaac_ros_apriltag_usb_cam.launch.py
