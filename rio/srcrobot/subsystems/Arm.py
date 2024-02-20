bbfbdc09d2fc06bf90be48caf655c96134cc5c64
itdependz <samarthdas81@gmail.com>
2024-02-20 00:30:09 +0000

    fixed arm

--- rio/srcrobot/subsystems/Arm.py
+++ rio/srcrobot/subsystems/Arm.py
@@ -1,17 +1,19 @@
 from commands2.subsystem import Subsystem
-from commands2.subsystem import Commands
+from commands2.subsystem import Command
+from commands2 import WaitCommand
+from commands2 import WaitUntilCommand
 import wpimath.filter
 import wpimath
 import wpilib
-import phoenix6
+import phoenix5
 import math
 
-# class Arm(Subsystem):
+class Arm(Subsystem):
 
-#     # 
-#     #    The motion magic parameters are configured in slot 0. Slot 1 is configured
-#     #    for velocity control for zeroing.
-#     #    
+    # 
+    #    The motion magic parameters are configured in slot 0. Slot 1 is configured
+    #    for velocity control for zeroing.
+    #    
 
 
     def __init__(self):
@@ -36,28 +38,28 @@ import math
         self.kZeroEncoderVelocity = -self.kEncoderTicksPerDegreeOfArmMotion * 5.0
         self.kZeroingWaitForMoveSec = 2.0
         self.ZeroingVelocityTolerance = 10.0
+        
 
+        self.armMotor = phoenix5.TalonSRX(self.kArmMotorCANId)
 
-        self.armMotor = phoenix6.TalonSRX(self.kArmMotorCANId)
+        # True when servo control active and false otherwise.
+        self.isServoControl = False
+        # The last requested servo target for target checking.
+        self.lastServoTarget = 0.0
+        self.restingAtZero = False
 
-#         # True when servo control active and false otherwise.
-#         self.isServoControl = False
-#         # The last requested servo target for target checking.
-#         self.lastServoTarget = 0.0
-#         self.restingAtZero = False
+        self.armMotor.setSensorPhase(True)
+        self.armMotor.config_kP(self.kMotionMagicSlot, self.kPMotionMagic)
+        self.armMotor.config_kI(self.kMotionMagicSlot, self.kIMotionMagic)
+        self.armMotor.config_kD(self.kMotionMagicSlot, self.kDMotionMagic)
+        self.armMotor.config_kF(self.kMotionMagicSlot, self.kF)
+        self.armMotor.configMotionCruiseVelocity(self.kCruiseVelocity)
+        self.armMotor.configMotionAcceleration(self.kMaxAccel)
 
-#         self.armMotor.setSensorPhase(True)
-#         self.armMotor.config_kP(self.kMotionMagicSlot, self.kPMotionMagic)
-#         self.armMotor.config_kI(self.kMotionMagicSlot, self.kIMotionMagic)
-#         self.armMotor.config_kD(self.kMotionMagicSlot, self.kDMotionMagic)
-#         self.armMotor.config_kF(self.kMotionMagicSlot, self.kF)
-#         self.armMotor.configMotionCruiseVelocity(self.kCruiseVelocity)
-#         self.armMotor.configMotionAcceleration(self.kMaxAccel)
-
-#         self.armMotor.config_kP(self.kVelocitySlot, self.kPVelocity)
-#         self.armMotor.config_kI(self.kVelocitySlot, 0.0)
-#         self.armMotor.config_kD(self.kVelocitySlot, 0.0)
-#         self.armMotor.config_kF(self.kVelocitySlot, self.kF)
+        self.armMotor.config_kP(self.kVelocitySlot, self.kPVelocity)
+        self.armMotor.config_kI(self.kVelocitySlot, 0.0)
+        self.armMotor.config_kD(self.kVelocitySlot, 0.0)
+        self.armMotor.config_kF(self.kVelocitySlot, self.kF)
         
 
     #     
@@ -68,17 +70,16 @@ import math
     #     @return a command that will move the arm toward 0, and stop when stalled.
     #    
     def seekArmZero(self):
+        
         return self.runOnce(lambda: self.selectPIDSlot(self.kVelocitySlot)).andThen(self.armMotor.set(
-            phoenix6.ControlMode.Velocity,
+            phoenix5.ControlMode.Velocity,
             self.kZeroEncoderVelocity,
-            phoenix6.DemandType.ArbitraryFeedForward,
+            phoenix5.DemandType.ArbitraryFeedForward,
             self.calculateGravityFeedForward())) \
-            .raceWith(Commands \
-                .waitSeconds(self.kZeroingWaitForMoveSec)
-                .andThen(self.detectStallAtHardStop())) \
-        .andThen(self.restingAtZero()) \
-        .withName("seekArmZero")
-
+            .raceWith(WaitCommand(self.kZeroingWaitForMoveSec) \
+            .andThen(self.detectStallAtHardStop())) \
+            .andThen(self.restingAtZero()) \
+            .withName("seekArmZero")
 
     #     
     #     Creates a command to detect stall at the hard stop during
@@ -87,80 +88,77 @@ import math
     #     @return the hard stop detection command.
     #    
     def detectStallAtHardStop(self):
-        stallDebouncer = wpimath.filter.Debouncer(1.0, wpimath.filter.Debouncer.DebounceType.kRising)
-        return Commands.waitUntil(lambda: stallDebouncer.calculate(wpimath.MathUtil.isNear(
-            0.0,
-            self.armMotor.getSelectedSensorVelocity(self.kVelocitySlot),
-            self.kZeroingVelocityTolerance)))
+        stallDebouncer = wpimath.filter.Debouncer(1.0, wpimath.filter.Debouncer.DebounceType.kRising)  
+        return WaitUntilCommand(lambda: abs(self.armMotor.getSelectedSensorVelocity(self.kVelocitySlot) - 0.0) < self.ZeroingVelocityTolerance)
         
 
-#     #     
-#     #     Should only be called as the last step of {@link #seekArmZero()}. The encoder
-#     #     is reset to 0.
-#     #     
-#     #     @return a command to rest at the hard stop at 0 and on target.
-#     #    
-#     def restingAtZero(self):
-#         return self.runOnce(lambda: self.setRestingAtZero(True)) \
-#         .andThen(self.run(lambda: self.armMotor.set(0.0)).withTimeout(1.0)) \
-#         .andThen(lambda: self.hardSetEncoderToZero()) \
-#         .andThen(self.run(lambda: self.armMotor.set(-0.1))) \
-#         .finallyDo(lambda: self.setRestingAtZero(False))
+    #     
+    #     Should only be called as the last step of {@link #seekArmZero()}. The encoder
+    #     is reset to 0.
+    #     
+    #     @return a command to rest at the hard stop at 0 and on target.
+    #    
+    def restingAtZero(self):
+        return self.runOnce(lambda: self.setRestingAtZero(True)) \
+        .andThen(self.run(lambda: self.armMotor.set(0.0)).withTimeout(1.0)) \
+        .andThen(lambda: self.hardSetEncoderToZero()) \
+        .andThen(self.run(lambda: self.armMotor.set(-0.1))) \
+        .finallyDo(lambda: self.setRestingAtZero(False))
     
-#     def setRestingAtZero(self, restAtZero):
-#         self.restingAtZero = restAtZero
+    def setRestingAtZero(self, restAtZero):
+        self.restingAtZero = restAtZero
 
-#     def hardSetEncoderToZero(self):
-#         self.armMotor.setSelectedSensorPosition(0)
+    def hardSetEncoderToZero(self):
+        self.armMotor.setSelectedSensorPosition(0)
 
     
-#     #     
-#     #     Creates a command to servo the arm to a desired angle. Note that 0 is
-#     #     parallel to the ground. The entire operation is run with
-#     #     {@link m_isServoControl} set to true to enable on target checking. See
-#     #     {@link #isServoOnTarget(double)}.
-#     #     
-#     #     <p>
-#     #     If the target is 0 or less, the command from {@link #seekArmZero()} is
-#     #     returned.
-#     #     
-#     #     @param degrees the target degrees from 0. Must be positive.
-#     #     @return a command that will servo the arm and will not end until interrupted
-#     #             by another command.
-#     #    
-#     def servoArmToTarget(self, degrees) :
-#         if (degrees <= 0.0):
-#             return self.seekArmZero()
+    #     
+    #     Creates a command to servo the arm to a desired angle. Note that 0 is
+    #     parallel to the ground. The entire operation is run with
+    #     {@link m_isServoControl} set to true to enable on target checking. See
+    #     {@link #isServoOnTarget(double)}.
+    #     
+    #     <p>
+    #     If the target is 0 or less, the command from {@link #seekArmZero()} is
+    #     returned.
+    #     
+    #     @param degrees the target degrees from 0. Must be positive.
+    #     @return a command that will servo the arm and will not end until interrupted
+    #             by another command.
+    #    
+    def servoArmToTarget(self, degrees) :
+        if (degrees <= 0.0):
+            return self.seekArmZero()
 
         targetSensorUnits = degrees * self.kEncoderTicksPerDegreeOfArmMotion
         return self.runOnce(lambda: self.initializeServoArmToTarget(degrees)) \
         .andThen(lambda: self.run(self.armMotor.set(
-            phoenix6.ControlMode.MotionMagic,
+            phoenix5.ControlMode.MotionMagic,
             targetSensorUnits,
-            phoenix6.DemandType.ArbitraryFeedForward,
+            phoenix5.DemandType.ArbitraryFeedForward,
             self.calculateGravityFeedForward()))) \
         .finallyDo(lambda: self.setServoControl(False)) \
         .withName("servoArmToTarget: " + degrees)
   
-#     def initializeServoArmToTarget(self, degrees):
-#         self.lastServoTarget = degrees
-#         self.setServoControl(True)
-#         self.selectPIDSlot(self.kMotionMagicSlot)
+    def initializeServoArmToTarget(self, degrees):
+        self.lastServoTarget = degrees
+        self.setServoControl(True)
+        self.selectPIDSlot(self.kMotionMagicSlot)
 
-#     def setServoControl(self, servoControl):
-#         self.isServoControl = servoControl
+    def setServoControl(self, servoControl):
+        self.isServoControl = servoControl
 
 
-#     #      
-#     #     This method should rarely be used. It is for pure manual control (no encoder
-#     #     usage) which should be avoided.
-#     #    
-#     #     @param percentOutput the commanded output [-1.0..1.0]. Positive is up.
-#     #     @return a command that drives the arm via double supplier.
-#     #    
-#     def moveArm(self, percentOutput):
-#         return self.run(lambda: self.armMotor.set(percentOutput() * 0.4)) \
-#         .withName("moveArm")
+    #      
+    #     This method should rarely be used. It is for pure manual control (no encoder
+    #     usage) which should be avoided.
+    #    
+    #     @param percentOutput the commanded output [-1.0..1.0]. Positive is up.
+    #     @return a command that drives the arm via double supplier.
+    #    
+    def moveArm(self, percentOutput):
+        return self.run(lambda: self.armMotor.set(percentOutput() * 0.4)) \
+        .withName("moveArm")
   
 
 
@@ -172,19 +170,19 @@ import math
     #    
     def calculateGravityFeedForward(self):
         degrees = self.getDegrees()
-        radians = math.toRadians(degrees)
+        radians = degrees * (math.pi/180.0)
         cosineScalar = math.cos(radians)
         return self.MaxGravityFF * cosineScalar
   
-#     #     
-#     #     Assuming a properly zeroed arm (0 degrees is parallel to the ground), return
-#     #     the current arm angle.
-#     #     
-#     #     @return the current arm angle in degrees from 0.
-#     #    
-#     def getDegrees(self):
-#         currentPos = self.armMotor.getSelectedSensorPosition()
-#         return (currentPos - self.kMeasuredTicksWhenHorizontal) / self.kEncoderTicksPerDegreeOfArmMotion
+    #     
+    #     Assuming a properly zeroed arm (0 degrees is parallel to the ground), return
+    #     the current arm angle.
+    #     
+    #     @return the current arm angle in degrees from 0.
+    #    
+    def getDegrees(self):
+        currentPos = self.armMotor.getSelectedSensorPosition()
+        return (currentPos - self.kMeasuredTicksWhenHorizontal) / self.kEncoderTicksPerDegreeOfArmMotion
 
 
     #    
@@ -196,31 +194,31 @@ import math
     def isServoOnTarget(self):
         return self.restingAtZero \
             or (self.isServoControl \
-                and math.MathUtil.isNear(self.lastServoTarget, self.getDegrees(), self.kServoToleranceDegrees))
+                and abs(self.lastServoTarget - self.getDegrees()) > self.kServoToleranceDegrees)
 
 
 
-#     #         
-#     #    Selects the specified slot for the next PID controlled arm movement. Always
-#     #    selected for primary closed loop control.
-#     #     
-#     #    @param slot the PID slot
-#     #    
-#     def selectPIDSlot(self, slot):
-#         self.armMotor.selectProfileSlot(slot, 0)
+    #         
+    #    Selects the specified slot for the next PID controlled arm movement. Always
+    #    selected for primary closed loop control.
+    #     
+    #    @param slot the PID slot
+    #    
+    def selectPIDSlot(self, slot):
+        self.armMotor.selectProfileSlot(slot, 0)
         
 
-#     #    
-#     #    Updates the dashboard with critical arm data.
-#     #    
+    #    
+    #    Updates the dashboard with critical arm data.
+    #    
   
-#     def periodic(self) :
-#         # TODO reduce this to essentials.
-#         wpilib.SmartDashboard.putNumber("Arm degrees", self.getDegrees())
-#         wpilib.SmartDashboard.putNumber("Arm current", self.armMotor.getStatorCurrent())
-#         wpilib.SmartDashboard.putBoolean("Arm on target", self.isServoOnTarget())
-#         currentCommand = self.getCurrentCommand()
-#         wpilib.SmartDashboard.putString("Arm command", currentCommand.getName() if currentCommand is not None else "<null>")
-#         wpilib.SmartDashboard.putNumber("Arm zeroing velocity", self.armMotor.getSelectedSensorVelocity(self.kVelocitySlot))
-#         wpilib.SmartDashboard.putBoolean("Arm resting", self.restingAtZero)
-#         wpilib.SmartDashboard.putBoolean("Servo control", self.isServoControl)
+    def periodic(self) :
+        # TODO reduce this to essentials.
+        wpilib.SmartDashboard.putNumber("Arm degrees", self.getDegrees())
+        wpilib.SmartDashboard.putNumber("Arm current", self.armMotor.getStatorCurrent())
+        wpilib.SmartDashboard.putBoolean("Arm on target", self.isServoOnTarget())
+        currentCommand = self.getCurrentCommand()
+        wpilib.SmartDashboard.putString("Arm command", currentCommand.getName() if currentCommand is not None else "<null>")
+        wpilib.SmartDashboard.putNumber("Arm zeroing velocity", self.armMotor.getSelectedSensorVelocity(self.kVelocitySlot))
+        wpilib.SmartDashboard.putBoolean("Arm resting", self.restingAtZero)
+        wpilib.SmartDashboard.putBoolean("Servo control", self.isServoControl)
