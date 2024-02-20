0e6b168b337e5dac3d8779c8fbac555772efcd89
nchan18 <nchan18@outook.com>
2024-02-19 22:01:06 -0500

    removed src

--- docker/developer/src/joint_trajectory_teleop/joint_trajectory_teleop/yaml_test.py
+++
@@ -1,7 +0,0 @@
-import yaml
-yaml_path = '/workspaces/roboeagles2024/src/edna_bringup/config/xbox-real.yaml'
-with open(yaml_path, 'r') as f:
-    yaml = yaml.safe_load(f)
-yaml = yaml['/*']['joint_trajectory_teleop_node']['ros__parameters']
-
-print(str(yaml['function_mapping']['elevator_loading_station']['button']))
